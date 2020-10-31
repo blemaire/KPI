9376c0ab67a792d394b6ec638caaad00cbec211a
blemaire <bastien.lemaire@gmail.com>
2020-10-31 20:32:46 +0100

    Improve return type for swagger

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,14 +1,27 @@
 import {Controller, Get, Query} from '@nestjs/common';
+import {ApiProperty, ApiResponse} from '@nestjs/swagger';
 import {ApiModelPropertyOptional} from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import * as investments from './data/dataset.json';
-import {IInvestment} from './data/investement.interface';
+import {Investment} from './data/investement.interface';
 
 class SearchParams {
-  @ApiModelPropertyOptional()
+  @ApiModelPropertyOptional({
+    description: 'The city of the investment',
+  })
   ville: string;
 
   @ApiModelPropertyOptional()
   etat_d_avancement: string;
+}
+
+class SearchResponse {
+  @ApiProperty({
+    type: [Investment],
+  })
+  data: Investment[];
+
+  @ApiProperty()
+  total: number;
 }
 
 @Controller('investments')
@@ -16,18 +29,30 @@ export class AppController {
   constructor() {
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'The investments matching the criteria. All results are returned if no criteria are given.',
+    type: SearchResponse,
+  })
   @Get()
-  getFiltered(@Query() params: SearchParams): IInvestment[] {
+  getFiltered(@Query() params: SearchParams): SearchResponse {
     if (!params.ville && !params.etat_d_avancement) {
-      return investments;
+      return this.sanitise(investments);
     }
 
-    params.ville = params.ville || '';
-    params.etat_d_avancement = params.etat_d_avancement || '';
-
-    return investments.filter(investment => {
+    return this.sanitise(investments.filter(investment => {
+      const ville = params.ville || '';
+      const etat_d_avancement = params.etat_d_avancement || '';
+      
       // TODO we could add an additional flag change the OR search into and AND search where both criteria need to match
-      return investment.ville === params.ville || investment.etat_d_avancement === params.etat_d_avancement;
-    });
+      return investment.ville === ville || investment.etat_d_avancement === etat_d_avancement;
+    }));
+  }
+
+  private sanitise(data: Investment[]): SearchResponse {
+    return {
+      data,
+      total: data.length,
+    };
   }
 }
--- packages/server/src/data/investement.interface.ts
+++ packages/server/src/data/investement.interface.ts
@@ -1,21 +1,42 @@
-export interface IInvestment {
+import {ApiProperty} from '@nestjs/swagger';
+
+export class Investment {
+  @ApiProperty()
   annee_d_individualisation?: string;
+  @ApiProperty()
   annee_de_livraison?: string;
+  @ApiProperty()
   cao_attribution?: string;
+  @ApiProperty()
   codeuai?: string;
+  @ApiProperty()
   entreprise?: string;
+  @ApiProperty()
   enveloppe_prev_en_meu?: number;
+  @ApiProperty()
   etat_d_avancement?: string;
+  @ApiProperty()
   latitude?: number;
+  @ApiProperty()
   longitude?: number;
+  @ApiProperty()
   lycee?: string;
+  @ApiProperty()
   maitrise_d_oeuvre?: string;
+  @ApiProperty()
   mandataire?: string;
+  @ApiProperty()
   mode_de_devolution?: string;
+  @ApiProperty()
   montant_des_ap_votes_en_meu?: number;
+  @ApiProperty()
   nombre_de_lots?: number;
+  @ApiProperty()
   notification_du_marche?: string;
+  @ApiProperty()
   ppi?: string;
+  @ApiProperty()
   titreoperation?: string;
+  @ApiProperty()
   ville?: string;
 }
