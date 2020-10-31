9e72ebf0f402cafc838ba30f2287ff16d63eaa13
blemaire <bastien.lemaire@gmail.com>
2020-10-31 16:03:48 +0100

    Added first search parameter

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,14 +1,32 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, Query} from '@nestjs/common';
 import {AppService} from './app.service';
-import * as data from './data/dataset.json';
+import * as investments from './data/dataset.json';
 import {InvestmentInterface} from './data/investment.interface';
 
 @Controller('investments')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getAll(): InvestmentInterface[] {
-    return data;
+    return investments.sort((a, b) => {
+      if (a.ville > b.ville) {
+        return 1;
+      }
+
+      if (a.ville < b.ville) {
+        return -1;
+      }
+
+      return 0;
+    });
+  }
+
+  @Get('search')
+  getFiltered(@Query('ville') ville?: string): InvestmentInterface[] {
+    return investments.filter(investment => {
+      return investment.ville === ville;
+    });
   }
 }
