ffc7414cf69a5aa6ed99ca10d6347dc05999d553
blemaire <bastien.lemaire@gmail.com>
2020-10-31 22:09:24 +0100

    Added search by ID

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,5 +1,5 @@
-import {Controller, Get, Query} from '@nestjs/common';
-import {ApiProperty, ApiResponse} from '@nestjs/swagger';
+import {Controller, Get, Param, Query} from '@nestjs/common';
+import {ApiOkResponse, ApiProperty} from '@nestjs/swagger';
 import {ApiModelPropertyOptional} from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import * as investments from './data/dataset.json';
 import {Investment} from './data/investement.interface';
@@ -10,7 +10,9 @@ class SearchParams {
   })
   ville: string;
 
-  @ApiModelPropertyOptional()
+  @ApiModelPropertyOptional({
+    description: 'The current state of the investment'
+  })
   etat_d_avancement: string;
 }
 
@@ -29,27 +31,35 @@ export class AppController {
   constructor() {
   }
 
-  @ApiResponse({
-    status: 200,
-    description: 'The investments matching the criteria. All results are returned if no criteria are given.',
+  @ApiOkResponse({
     type: SearchResponse,
+    description: 'Search investments by criteria. Returns all results if no criteria given.',
   })
   @Get()
-  getFiltered(@Query() params: SearchParams): SearchResponse {
+  public getFiltered(@Query() params: SearchParams): SearchResponse {
     if (!params.ville && !params.etat_d_avancement) {
-      return this.sanitise(investments);
+      return this.formatForList(investments);
     }
 
-    return this.sanitise(investments.filter(investment => {
+    return this.formatForList(investments.filter(investment => {
       const ville = params.ville || '';
       const etat_d_avancement = params.etat_d_avancement || '';
-      
+
       // TODO we could add an additional flag change the OR search into and AND search where both criteria need to match
       return investment.ville === ville || investment.etat_d_avancement === etat_d_avancement;
     }));
   }
 
-  private sanitise(data: Investment[]): SearchResponse {
+  @ApiOkResponse({
+    type: Investment,
+    description: 'Get an investment by codeuai.',
+  })
+  @Get(':codeuai')
+  public getById(@Param('codeuai') codeuai: string): Investment | null {
+    return investments.find(investment => investment.codeuai === codeuai);
+  }
+
+  private formatForList(data: Investment[]): SearchResponse {
     return {
       data,
       total: data.length,
